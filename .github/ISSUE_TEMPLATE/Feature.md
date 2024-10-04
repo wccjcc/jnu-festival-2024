44bb7383bb5ce41acc4b47c018b513eb5f2aeaf0
cosmos-1885 <dmsrkql104321@gmail.com>
2024-10-04 19:44:56 +0900

    feat: initialize project setting

---
+++ .github/ISSUE_TEMPLATE/Feature.md
@@ -0,0 +1,21 @@
+---
+name: Feature
+about: 기능 개발 사항을 작성해주세요!
+title: "[Feature]"
+labels: ''
+assignees: ''
+
+---
+
+## Title
+제목을 작성하세요!
+
+## Description
+설명을 작성하세요!
+
+## To Do
+- [ ] 내용
+- [ ] 내용
+
+## Reference
+참고 자료를 작성해주세요!
---
+++ src/test/java/com/jnu/festival/FestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnu.festival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
