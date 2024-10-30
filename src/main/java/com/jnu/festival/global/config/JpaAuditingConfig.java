5aae3f0e22fc086642da4bc02f81a74aa2574660
cosmos-1885 <dmsrkql104321@gmail.com>
2024-10-31 06:56:51 +0900

    feat: JpaAuditingConfig 분리

---
+++ src/main/java/com/jnu/festival/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.jnu.festival.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
